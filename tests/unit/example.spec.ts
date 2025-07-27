5696cb10b1faea8003281181d4e276b43bec0cc4
MIGUELSOBERANOCARRANZA <MEEL0588@gmail.com>
2025-07-27 00:13:19 -0600

    feat: reemplazar Tab1Page con home.vue y eliminar archivos obsoletos

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,8 +1,8 @@
 import { mount } from '@vue/test-utils'
-import Tab1Page from '@/views/Tab1Page.vue'
+import Tab1Page from '@/views/home.vue'
 import { describe, expect, test } from 'vitest'
 
-describe('Tab1Page.vue', () => {
+describe('home.vue', () => {
   test('muestra mensaje de bienvenida', () => {
     const wrapper = mount(Tab1Page)
     expect(wrapper.text()).toMatch('Bienvenido a Totopos Garchi')
