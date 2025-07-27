9bbe6bc106372ff11ff248f0b9eb2ecff3fbc07d
Miguel Soberano <meel0588@gmail.com>
2025-07-27 00:21:07 -0600

    feat: modernize home page

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -5,6 +5,6 @@ import { describe, expect, test } from 'vitest'
 describe('home.vue', () => {
   test('muestra mensaje de bienvenida', () => {
     const wrapper = mount(Tab1Page)
-    expect(wrapper.text()).toMatch('Bienvenido a Totopos Garchi')
+    expect(wrapper.text()).toMatch('Totopos para conquistar tu paladar')
   })
 })
